aec8afd975b383659cb7741ef67ae3f5359857c9
SeemantaDebdas <seemantadebdas@gmail.com>
2022-02-19 19:07:05 +0530

    Added animation to attack and hit event

--- Hack and Slash RPG/Assets/Scripts/Combat/Fighter.cs
+++ Hack and Slash RPG/Assets/Scripts/Combat/Fighter.cs
@@ -9,10 +9,24 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour,IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float weaponDamage = 10f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+
         Transform target;
+
+        Animator anim;
+        int AttackTrigger = Animator.StringToHash("AttackTrigger");
+
+        float timeSinceLastAttack = 0;
+        private void Awake()
+        {
+            anim = GetComponent<Animator>();
+        }
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
 
             if (!(Vector3.Distance(transform.position, target.position) <= weaponRange))
@@ -22,6 +36,17 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().CancelAction();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack >= timeBetweenAttacks)
+            {
+                anim.SetTrigger(AttackTrigger);
+                timeSinceLastAttack = 0;
+                target.GetComponent<Health>().TakeDamage(10f);
             }
         }
 
@@ -35,8 +60,14 @@ namespace RPG.Combat
 
         public void CancelAction()
         {
-            //cancel this action
             CancelAttack();
+        }
+
+        //Animation Event
+        void Hit()
+        {
+            if (target != null)
+                target.GetComponent<Health>().TakeDamage(weaponDamage);
         }
     }
 }
