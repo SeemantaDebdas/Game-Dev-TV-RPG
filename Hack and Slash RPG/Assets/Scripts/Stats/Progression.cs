5ce9fb9670e0fdf5839a134c5165d2752759fa59
SeemantaDebdas <seemantadebdas@gmail.com>
2022-06-12 18:21:47 +0530

    Adding Experience Reward with Health in Character Stats

--- Hack and Slash RPG/Assets/Scripts/Stats/Progression.cs
+++ Hack and Slash RPG/Assets/Scripts/Stats/Progression.cs
@@ -13,17 +13,25 @@ namespace RPG.Stats
             foreach(ProgressionCharacterClass progressionClass in characterClasses)
             {
                 if (progressionClass.characterClass == characterClass)
-                    return progressionClass.health[level - 1];
+                {
+                    //return progressionClass.health[level - 1];
+                }
             }
             return 0f;
         }
-
 
         [System.Serializable]
         class ProgressionCharacterClass
         {
             public CharacterClass characterClass;
-            public float[] health;
+            public ProgressionStat[] stats;
+        }
+
+        [System.Serializable]
+        class ProgressionStat
+        {
+            public Stat stat;
+            public float[] levels;
         }
     }
 }
